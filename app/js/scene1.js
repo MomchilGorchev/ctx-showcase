02d23fbe873112bbfa7ec47bab9dd8778eaf396f
Momchil Gorchev <momchil.gorchev@gmail.com>
2016-03-25 15:43:43 +0000

    Replacing <pre> with JSBin iframe

--- app/js/scene1.js
+++ app/js/scene1.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-
     'use strict';
     window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
     window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
@@ -13,24 +12,28 @@ document.addEventListener('DOMContentLoaded', function(){
     canvas.width = '1000';
     canvas.height = '500';
 
+    // Utility random number generator fn
+    // Returns a number between tow passed values, e.g min and max
+    function randomNumber(min, max) {
+        return Math.random() * (max - min) + min;
+    }
 
+    // Create some points
     function createDots(amount){
         for(var i = 0; i < amount; i++){
 
             var x = Math.random() * canvas.width,
                 y = Math.random() * canvas.height,
                 radius = Math.random() * 10,
-                color = colors[Math.floor(i%colors.length)],
-                alpha = randomNumber(0.3, 1);
-            var dot = new Dot(x, y, radius, color, alpha);
+                color = colors[Math.floor(i % colors.length)],
+                alpha = randomNumber(0.3, 1),
+                dot = new Dot(x, y, radius, color, alpha);
 
             dots.push(dot);
         }
-
     }
 
-    createDots(1000);
-
+    // Dot shape constructor
     function Dot(x, y, radius, color, alpha){
 
         var _this = this;
@@ -44,36 +47,36 @@ document.addEventListener('DOMContentLoaded', function(){
             ctx.beginPath();
             ctx.arc(_this.x, _this.y, _this.radius, 0, Math.PI * 2, true);
             ctx.closePath();
-            ctx.fillStyle = 'rgba('+_this.color+', '+_this.alpha+')';
+            ctx.fillStyle = 'rgba('+ _this.color +', '+ _this.alpha +')';
             ctx.fill();
         };
 
+        // [DEBUG]
         //console.log(this);
     }
 
+    // Loop function to be passed to requestAnimationFrame
     function loop(){
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-
         for(var j = 0; j < dots.length; j++){
             dots[j].draw(ctx);
         }
-
         window.requestAnimationFrame(loop);
     }
 
-    loop();
-
-    function randomNumber(min, max) {
-        return Math.random() * (max - min) + min;
-    }
-
+    // Movement animation
     function moveAround(dot){
 
         var pos = {
             x: dot.x,
             y: dot.y
         };
+
+        // The newPos values determine the new position of the object
+        // Currently is set to pick a random spot on the canvas
+        // Experiment with this values as well as the animation timing values 
+        // to achieve different effect
         var newPos = {
             x: Math.random() * canvas.width,
             y: Math.random() * canvas.height
@@ -98,8 +101,14 @@ document.addEventListener('DOMContentLoaded', function(){
         });
     }
 
-    for(var k = 0; k < dots.length; k++){
-        moveAround(dots[k]);
+    // Init the sequence
+    function init(){
+        createDots(1000);
+        loop();
+        for(var k = 0; k < dots.length; k++){
+            moveAround(dots[k]);
+        }
     }
-
+    
+    init();
 });
